e484dd8b38b928dd5aaf3c3dc36af5acd4855138
rohit-singh-bisht <thersbisht@gmail.com>
2023-10-24 21:56:04 +0530

    delte user from mock data using fs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 9000;
+const fs = require("fs");
 const users = require("./MOCK_DATA.json");
 
 app.get("/users", (req, res) => {});
@@ -13,6 +14,21 @@ app.get("/api/users/:id", (req, res) => {
   const userId = Number(req.params.id);
   const userData = users?.find((user) => user?.id === userId);
   return res.json(userData);
+});
+
+app.delete("/api/users/:id", (req, res) => {
+  const userId = Number(req.params.id);
+  const userFound = users?.find((user) => user?.id === userId);
+  if (!userFound) {
+    return res.send("User not found to delete");
+  }
+  const userList = users.filter((user) => user?.id !== userId);
+  fs.writeFile("MOCK_DATA.json", JSON.stringify(userList), (err, data) => {
+    if (err) {
+      return console.log("Can't delete user successfully");
+    }
+    return res.status(200).send("User Deleted Successfully");
+  });
 });
 
 app.listen(port, () => {
