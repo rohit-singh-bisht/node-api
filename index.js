8955a13604b352473d69231c0f0c8dee1cac6423
rohit-singh-bisht <thersbisht@gmail.com>
2023-10-25 07:17:48 +0530

    adding new user to mock data

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = 9000;
 const fs = require("fs");
 const users = require("./MOCK_DATA.json");
 
+// Body parsing middleware (for JSON and URL-encoded data)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/users", (req, res) => {});
 
 app.get("/api/users", (req, res) => {
@@ -29,6 +33,40 @@ app.delete("/api/users/:id", (req, res) => {
     }
     return res.status(200).send("User Deleted Successfully");
   });
+});
+
+app.post("/api/users", (req, res) => {
+  const { first_name, last_name, email, gender, job_title } = req.body;
+  if (!first_name || !last_name || !email || !gender || !job_title)
+    return res
+      .status(404)
+      .send("First name, last name, email, gender and job title are required");
+  const emailExist = users.find((user) => user?.email === email);
+  if (emailExist)
+    return res
+      .status(404)
+      .send("Email already exist. Please try with another email");
+  const copiedUsersArray = [...users];
+  const userBody = {
+    id: Math.max(...copiedUsersArray?.map((users) => users?.id)) + 1,
+    first_name,
+    last_name,
+    email,
+    gender,
+    job_title,
+  };
+  const updatedUserArray = [...copiedUsersArray, userBody];
+  fs.writeFile(
+    "MOCK_DATA.json",
+    JSON.stringify(updatedUserArray),
+    (err, data) => {
+      if (err) {
+        console.log("err", err);
+        return res.status(400).send("Error adding user.");
+      }
+      res.status(200).send("User added successfully");
+    }
+  );
 });
 
 app.listen(port, () => {
